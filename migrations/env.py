20b6100d7010ed8863228357254238cbee106171
Elvinato <aimteso@gmail.com>
2023-02-16 12:35:39 +0400

    db test

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from config import DB_PORT, DB_HOST, DB_USER, DB_NAME, DB_PASS
+from models.models import metadata
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -26,7 +29,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
